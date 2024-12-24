fbb4b35c32b60f36884ab3e23500246b31dd3f1f
김현지 <chik1y@naver.com>
2024-12-24 11:32:10 +0900

    [fix] ResponseProperties에 @ConstructorBinding 추가 (#2)

--- myhands/src/main/java/tabom/myhands/MyhandsApplication.java
+++ myhands/src/main/java/tabom/myhands/MyhandsApplication.java
@@ -3,8 +3,11 @@ package tabom.myhands;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import tabom.myhands.common.properties.ResponseProperties;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
+@EnableConfigurationProperties(ResponseProperties.class)
 public class MyhandsApplication {
 
 	public static void main(String[] args) {
