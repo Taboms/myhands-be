4cbf7ef84a99ee5f5f3c03aa4b50c2e716bc7cdf
gyeongmann <hkc8857@naver.com>
2025-01-02 01:04:51 +0900

    [feat] 근태 에러 코드 추가 및 수정 (#7)
    - message 단순화
    - 필수 변수 누락 에러 (4006)

--- myhands/src/main/java/tabom/myhands/error/errorcode/DayOffErrorCode.java
+++ myhands/src/main/java/tabom/myhands/error/errorcode/DayOffErrorCode.java
@@ -8,10 +8,11 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum DayOffErrorCode implements ErrorCode {
     USER_ID_NOT_FOUND(4001, HttpStatus.NOT_FOUND, "User not found with the given ID"),
-    INVALID_DATE_RANGE(4002, HttpStatus.BAD_REQUEST, "The start {date/time} cannot be after the end {date/time}"),
+    INVALID_DATE_RANGE(4002, HttpStatus.BAD_REQUEST, "Input date (range) is invalid"),
     DUPLICATE_DAY_OFF_REQUEST(4003, HttpStatus.CONFLICT, "A day off request already exists for this date"),
     PERIOD_LIMIT_EXCEEDED(4004, HttpStatus.BAD_REQUEST, "The requested day off period is too long"),
-    INVALID_OFF_TYPE(4005, HttpStatus.BAD_REQUEST, "The offType must be FULL or HALF");
+    INVALID_OFF_TYPE(4005, HttpStatus.BAD_REQUEST, "The offType must be FULL or HALF"),
+    REQUIRED_VALUE_IS_NULL(4006, HttpStatus.BAD_REQUEST, "Required value is null");
 
     private final int code;
     private final HttpStatus httpStatus;
