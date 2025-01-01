252a85780bb2eeeffaa231b7385724e0e7bcf95b
gyeongmann <hkc8857@naver.com>
2025-01-02 01:05:35 +0900

    [feat] 근태 레포지토리 생성 (#7)
    - 불필요 레포지토리 함수 제거
    - 일차, 반차 레포지토리 생성

--- myhands/src/main/java/tabom/myhands/domain/dayOff/repository/FullOffRepository.java
+++ myhands/src/main/java/tabom/myhands/domain/dayOff/repository/FullOffRepository.java
@@ -1,11 +1,11 @@
 package tabom.myhands.domain.dayOff.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import tabom.myhands.domain.dayOff.entity.DayOff;
+import tabom.myhands.domain.dayOff.entity.FullOff;
 import tabom.myhands.domain.user.entity.User;
 
 import java.util.List;
 
-public interface DayOffRepository extends JpaRepository<DayOff, Long> {
-    List<DayOff> findDayOffsByUser(User user);
+public interface FullOffRepository extends JpaRepository<FullOff, Long> {
+    List<FullOff> findFullOffsByUser(User user);
 }
--- myhands/src/main/java/tabom/myhands/domain/dayOff/repository/HalfOffRepository.java
+++ myhands/src/main/java/tabom/myhands/domain/dayOff/repository/HalfOffRepository.java
@@ -1,11 +1,11 @@
 package tabom.myhands.domain.dayOff.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import tabom.myhands.domain.dayOff.entity.DayOff;
+import tabom.myhands.domain.dayOff.entity.HalfOff;
 import tabom.myhands.domain.user.entity.User;
 
 import java.util.List;
 
-public interface DayOffRepository extends JpaRepository<DayOff, Long> {
-    List<DayOff> findDayOffsByUser(User user);
+public interface HalfOffRepository extends JpaRepository<HalfOff, Long> {
+    List<HalfOff> findHalfOffsByUser(User user);
 }
