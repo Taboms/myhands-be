a04c9dd209425cee1bf77540d90402d4d23f8e56
gyeongmann <hkc8857@naver.com>
2025-01-04 00:23:53 +0900

    [feat] 근태 에러코드 수정 (#7)

--- myhands/src/main/java/tabom/myhands/error/errorcode/DayOffErrorCode.java
+++ myhands/src/main/java/tabom/myhands/error/errorcode/DayOffErrorCode.java
@@ -7,12 +7,11 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum DayOffErrorCode implements ErrorCode {
-    USER_ID_NOT_FOUND(4001, HttpStatus.NOT_FOUND, "User not found with the given ID"),
-    INVALID_DATE_RANGE(4002, HttpStatus.BAD_REQUEST, "Input date (range) is invalid"),
-    DUPLICATE_DAY_OFF_REQUEST(4003, HttpStatus.CONFLICT, "A day off request already exists for this date"),
-    PERIOD_LIMIT_EXCEEDED(4004, HttpStatus.BAD_REQUEST, "The requested day off period is too long"),
-    INVALID_OFF_TYPE(4005, HttpStatus.BAD_REQUEST, "The offType must be FULL or HALF"),
-    REQUIRED_VALUE_IS_NULL(4006, HttpStatus.BAD_REQUEST, "Required value is null");
+    REQUIRED_VALUE_IS_NULL(4001, HttpStatus.BAD_REQUEST, "Required value is null"),
+    INVALID_OFF_TYPE(4002, HttpStatus.BAD_REQUEST, "The offType must be FULL or HALF"),
+    INVALID_DATE_RANGE(4003, HttpStatus.BAD_REQUEST, "Input date (range) is invalid"),
+    DUPLICATE_DAY_OFF_REQUEST(4004, HttpStatus.CONFLICT, "A day off request already exists for this date"),
+    PERIOD_LIMIT_EXCEEDED(4005, HttpStatus.BAD_REQUEST, "The requested day off period is too long");
 
     private final int code;
     private final HttpStatus httpStatus;
